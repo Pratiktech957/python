5d2550ba8557553fabe382942adfe73b11b27ba8
manifestdev <mohantypratik257@gmail.com>
2025-07-28 23:14:01 +0530

    Add 1.basic.py

---
+++ python_basic/1.basic.py
@@ -0,0 +1,37 @@
+print("Hello world ")
+
+
+
+#variable is a container for storing the value string like raj,int like 10, float like 10.5
+#variable name should not start with number, should not contain space, should not be a keyword
+
+
+name = "Pratik"
+print("my name is ",name)
+
+age = 100
+print(age)
+
+price =10.5
+print(price)
+
+
+
+# boolean type data type there are basically two things true or false....
+
+is_done = false 
+print(type(is_done))
+print(type(age))
+
+
+# taking input from 
+take =int(input("Enter a number: "))
+if take>10:
+    print("Greater than 10")
+elif take<10:
+    print("Less than 10")
+else:
+    print("Equal to 10")
+print("End of the program")
+
+
