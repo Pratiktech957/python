ae8a6c715f46c55093506448a8bb0ebc275ba254
manifestdev <mohantypratik257@gmail.com>
2025-07-29 00:03:57 +0530

    Add 2.basic.py

---
+++ python_basic/2.basic.py
@@ -0,0 +1,31 @@
+# operator 
+
+
+# rule remeber pedmas 
+
+a = 10
+b = 3
+
+# Arithmetic
+print(a + b, a - b, a * b, a / b, a % b, a ** b, a // b)
+
+# Assignment
+a += 5
+print(a)
+
+# Comparison
+print(a > b, a == b)
+
+# Logical
+print(a > 5 and b < 5)
+
+# Bitwise
+print(a & b, a | b)
+
+# Membership
+print('a' in 'banana')
+
+# Identity
+x = [1, 2]
+y = x
+print(x is y)
