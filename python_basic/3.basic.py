316bc693814c2839ed7e9b060ec38b44018aac2b
manifestdev <mohantypratik257@gmail.com>
2025-07-29 00:06:07 +0530

    Add 3.basic.py

---
+++ python_basic/3.basic.py
@@ -0,0 +1,19 @@
+# conditionals.....
+
+
+rad= float(input("Enter Your Radious:"))
+circle_area = 3.14 * rad * rad
+print("Area Of The Cicle Is :",circle_area)
+
+
+# start Conditionals 
+
+user_age = int(input("Enter Your Age:"))
+if user_age >18:
+    print("you are eligible for vote")
+elif user_age <=18:
+    print("You are not eligible for vote ")
+else:
+    print("Enter a Valid Age")
+    
+print("hi")
